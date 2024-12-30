d9a2e831e7dbf6a2a4ea3cf5c48742552fd4cb2b
Volha <kresiko@gmail.com>
2024-12-30 14:40:00 +0200

    completed

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -566,10 +566,19 @@ function propagateItemsByPositionIndex(arr) {
  *    shiftArray(['a', 'b', 'c', 'd'], -1) => ['b', 'c', 'd', 'a']
  *    shiftArray([10, 20, 30, 40, 50], -3) => [40, 50, 10, 20, 30]
  */
-function shiftArray(/* arr, n */) {
-  throw new Error('Not implemented');
-}
+function shiftArray(arr, n) {
+  if (arr.length === 0) return arr;
 
+  const { length } = arr;
+  const shift = ((n % length) + length) % length;
+
+  const [shiftedPart, remainingPart] = [
+    arr.slice(-shift),
+    arr.slice(0, -shift),
+  ];
+
+  return shiftedPart.concat(remainingPart);
+}
 /**
  * Sorts digit names.
  *
@@ -583,8 +592,22 @@ function shiftArray(/* arr, n */) {
  *   sortDigitNamesByNumericOrder([ 'nine','eight','nine','eight' ]) => [ 'eight','eight','nine','nine']
  *   sortDigitNamesByNumericOrder([ 'one','one','one','zero' ]) => [ 'zero','one','one','one' ]
  */
-function sortDigitNamesByNumericOrder(/* arr */) {
-  throw new Error('Not implemented');
+function sortDigitNamesByNumericOrder(arr) {
+  const digitOrder = [
+    'zero',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+  ];
+  return arr.slice().sort((a, b) => {
+    return digitOrder.indexOf(a) - digitOrder.indexOf(b);
+  });
 }
 
 /**
@@ -606,8 +629,20 @@ function sortDigitNamesByNumericOrder(/* arr */) {
  *   swapHeadAndTail([]) => []
  *
  */
-function swapHeadAndTail(/* arr */) {
-  throw new Error('Not implemented');
+function swapHeadAndTail(arr) {
+  const { length } = arr;
+  if (length <= 1) return arr;
+
+  const half = Math.floor(length / 2);
+
+  const head = arr.slice(0, half);
+  const tail = arr.slice(-half);
+
+  if (length % 2 === 0) {
+    return [...tail, ...head];
+  }
+  const middle = arr[half];
+  return [...tail, middle, ...head];
 }
 
 module.exports = {
